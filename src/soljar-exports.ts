00296cab2153d6a358d294fbc5cf48d5f51d2254
nitishxyz <nitishxyz@proton.me>
2025-03-22 18:48:29 +0530

    updated frontend to use latest Program declaration

--- src/soljar-exports.ts
+++ src/soljar-exports.ts
@@ -11,24 +11,14 @@ export { Soljar, SoljarIDL };
 export const SOLJAR_PROGRAM_ID = new PublicKey(SoljarIDL.address);
 
 // This is a helper function to get the Soljar Anchor program.
-export function getSoljarProgram(
-  provider: AnchorProvider,
-  address?: PublicKey
-) {
-  return new Program(
-    {
-      ...SoljarIDL,
-      address: address ? address.toBase58() : SoljarIDL.address,
-    } as Soljar,
-    provider
-  );
+export function getSoljarProgram(provider: AnchorProvider) {
+  return new Program<Soljar>(SoljarIDL, provider);
 }
 
 // This is a helper function to get the program ID for the Soljar program depending on the cluster.
 export function getSoljarProgramId(cluster: Cluster) {
   switch (cluster) {
     case "devnet":
-    case "testnet":
       // This is the program ID for the Soljar program on devnet and testnet.
       return new PublicKey("JARSq9S9RgyynuAwcdWh2yEG6MbhfntWq7zjXjAo87uQ");
     case "mainnet-beta":
